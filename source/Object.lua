e9e15afacd6e81fa672f8fa0fe793fb9f7f76bcb
Steve Dekorte <steve@StevesLaptop-7.local>
2009-04-22 21:04:48 -0700

    latest

--- source/Object.lua
+++ source/Object.lua
@@ -9,17 +9,33 @@ function Object:clone()
 end
 
 function Object:print()
-	for i, v in self do
-		print()
+	print("Object")
+	for i, v in pairs(self) do
+		print("  " .. i .. ":" .. v);
 	end
+	return self;
 end
 
 function Object:println()
 	self:print()
 	print("\n")
+	return self;
 end
 
 function Object:updateSlot(name, value)
-	self[name] = value
+	if (self["_" .. name]) then
+		self["_" .. name] = value;
+	else
+		print("updateSlot Error");
+	end
+	return value;
 end
 
+function Object:newSlot(name, value)
+	self["_" .. name] = value;
+	return self;
+end
+
+function Object:getSlot(name, value)
+	return self["_" .. name];
+end
